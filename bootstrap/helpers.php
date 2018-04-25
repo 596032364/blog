5f1e68d51eaf37fdf895488c745e1d49a5ca39dd
596032364 <596032364@qq.com>
2018-04-25 10:23:23 +0800

    创建布局文件

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -2,3 +2,8 @@
 /*
  * 公众使用函数
  */
+
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+/**前台*/
+Route::namespace('Home')->group(function () {
+    Route::get('/', 'PagesController@root')->name('root');
 });
